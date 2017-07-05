74a2f71e456d809c5885b90fc9fdc711250f0e6a
mangelosalvio <msalvio.13@gmail.com>
2017-07-05 16:41:12 +0800

    Add Delete Users

--- database/migrations/2017_07_05_163134_add_username_to_users.php
+++ database/migrations/2017_07_05_163134_add_username_to_users.php
@@ -14,6 +14,7 @@ class AddUsernameToUsers extends Migration
     {
         Schema::table('users', function (Blueprint $table){
             $table->string('username');
+            $table->softDeletes();
         });
     }
 
